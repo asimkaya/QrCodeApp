27f32cccab58b3cb890d2e6248ed20bf4fe3034d
Asım Emre Kaya <asim.emre.kaya@gmail.com>
2021-01-30 23:24:11 +0300

    blob to image

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { finalize } from 'rxjs/operators';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,49 @@ import { finalize } from 'rxjs/operators';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  quote: string | undefined;
-  isLoading = false;
+  radioSelect: any;
+  inputModel: any;
+  imageToShow: any;
+  isImageLoading: any;
 
-  constructor() {}
+  seasons: string[] = ['50x50', '100x100', '200x200', '300x300'];
+  radioGroupValue = [
+    { value: 1, display: '50x50' },
+    { value: 2, display: '100x100' },
+    { value: 3, display: '200x200' },
+    { value: 4, display: '300x300' },
+  ];
+
+  constructor(private api: ApiService) {}
 
   ngOnInit() {}
+
+  createImageFromBlob(image: Blob) {
+    let reader = new FileReader();
+    reader.addEventListener(
+      'load',
+      () => {
+        this.imageToShow = reader.result;
+      },
+      false
+    );
+
+    if (image) {
+      reader.readAsDataURL(image);
+    }
+  }
+
+  generateQrCode() {
+    this.isImageLoading = true;
+    this.api.getQrCode(this.inputModel, this.radioSelect).subscribe(
+      (data) => {
+        this.createImageFromBlob(data);
+        this.isImageLoading = false;
+      },
+      (error) => {
+        this.isImageLoading = false;
+        console.log(error);
+      }
+    );
+  }
 }

6f0d2eb2a086b8c07bf00964eeba5b59eb85fa13
Asım Emre Kaya <asim.emre.kaya@gmail.com>
2021-01-30 23:18:49 +0300

    responseType blob

--- src/app/home/api.service.ts
+++ src/app/home/api.service.ts
@@ -1,8 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getQrCode(text: string, size: string): Observable<Blob> {
+    return this.http.get('http://api.qrserver.com/v1/create-qr-code/?data=' + text + '!&size=' + size, {
+      responseType: 'blob',
+    });
+  }
 }

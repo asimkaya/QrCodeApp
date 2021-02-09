50445768f6c3cb461c8abb6aa50b136b4e7ce218
Asım Emre Kaya <asim.emre.kaya@gmail.com>
2021-02-09 16:41:55 +0300

    env serverUrl changed

--- src/app/home/api.service.ts
+++ src/app/home/api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '@env/environment';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,7 +10,7 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getQrCode(text: string, size: string): Observable<Blob> {
-    return this.http.get('http://api.qrserver.com/v1/create-qr-code/?data=' + text + '!&size=' + size, {
+    return this.http.get(environment.serverUrl + '?data=' + text + '!&size=' + size, {
       responseType: 'blob',
     });
   }
